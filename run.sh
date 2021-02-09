bac560ba402f39eff080c77d4c644807831337fb
nimashiri <nimashiri2012@gmail.com>
2021-02-09 17:50:27 -0500

    bash updated

--- run.sh
+++ run.sh
@@ -10,3 +10,16 @@ targetBuggyDir='/home/nimashiri/vsprojects/mutation_analysis/postgres/buggy'
 targetCleanDir='/home/nimashiri/vsprojects/mutation_analysis/postgres/clean'
 
 python3 mutatePostgre.py REDAWN $project_name $mutantSourcesDir $targetBuggyDir $targetCleanDir
+
+
+
+
+python3 analyze.py /eecs/home/nshiri/nima/postgres-REL_12_5/src
+
+project_name=postgre-12-5
+mutantSourcesDir='/eecs/home/nshiri/nima/postgres-REL_12_5/src'
+targetBuggyDir='/eecs/home/nshiri/nima/mutation_analysis/postgres/buggy'
+targetCleanDir='/eecs/home/nshiri/nima/mutation_analysis/postgres/clean'
+
+python3 mutatePostgre.py REDAWN $project_name $mutantSourcesDir $targetBuggyDir $targetCleanDir
+
